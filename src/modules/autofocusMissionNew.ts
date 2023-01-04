fa59496908e2e13933d7ac550acd55a0aec0e3d3
Niklas <84899746+Notme112@users.noreply.github.com>
2023-01-04 13:57:32 +0100

    added autofocusMissionNew

--- src/modules/autofocusMissionNew.ts
+++ src/modules/autofocusMissionNew.ts
@@ -3,13 +3,7 @@ import {
 } from "../types/codebase";
 
 export async function autofocusMissionNew(s: ReSiCodebaseSettingsType): Promise<void> {
-    if (location.href.includes('/missionNew/'))
-        document.addEventListener('keyup', (e) => {
-            if (e.key.toLowerCase() == s.resetAAO.keyToReset) {
-                document.querySelectorAll<HTMLElement>('#mission-vehicle-group-by-vehicle .mission-vehicle.vehicle.mission-vehicle-selected')?.forEach(e => {
-                    e.click()
-                });
-                document.querySelectorAll<HTMLElement>('.mission-aao-available').forEach(e => e.removeAttribute('aaocount'))
-            }
-        })
+    if (location.href.includes('/missionNew/')){
+        document.querySelector<HTMLElement>('#' + s.autofocusMissionNewOptions.field)?.focus();
+    }
 }
