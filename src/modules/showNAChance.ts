8fb4ec61989a2f7a3deb998602bba64945f1d182
Niklas <84899746+Notme112@users.noreply.github.com>
2023-03-13 16:31:09 +0100

    bei nur einem Pat wird nur einer angezeigt und nicht 1-1

--- src/modules/showNAChance.ts
+++ src/modules/showNAChance.ts
@@ -7,7 +7,7 @@ export async function showNAChance(s: ReSiCodebaseSettingsType) {
     const data = (await getAPI('missions', false))[parseInt(document.querySelector('.detail-title')?.getAttribute('missionid') || '0')];
     let newElement = document.createElement('span');
     // @ts-ignore
-    newElement.innerHTML = `Grundvariante: ${data.patients.min}-${data.patients.max} Patienten, ${data.patients.naChance}\% NA-Wahrscheinlichkeit`
+    newElement.innerHTML = `Grundvariante: ${data.patients.min == data.patients.max ? data.patients.min : data.patients.min + '-' + data.patients.max} Patienten, ${data.patients.naChance}\% NA-Wahrscheinlichkeit`
     newElement.classList.add('label', 'label-info')
     if (data.patients) document.querySelector('#s5')?.after(newElement)
 }
